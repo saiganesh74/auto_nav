37ccf99e5c75b4bb78471bb70a89055f3cf7b603
saiganesh74 <ganeshrejeti1@gmail.com>
2025-06-16 21:01:37 +0530

    New changes

--- src/auto_nav_gazebo/launch/gazebo.launch.py
+++ src/auto_nav_gazebo/launch/gazebo.launch.py
@@ -1,19 +1,21 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir
+from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.substitutions import FindPackageShare
 import os
 
 def generate_launch_description():
-    pkg_share = FindPackageShare(package='auto_nav_gazebo').find('auto_nav_gazebo')
+    pkg_share = FindPackageShare('auto_nav_gazebo').find('auto_nav_gazebo')
     world_path = os.path.join(pkg_share, 'worlds', 'my_college_world.world')
 
     return LaunchDescription([
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                FindPackageShare('gazebo_ros'), '/launch/gazebo.launch.py'
-            ]),
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    FindPackageShare('gazebo_ros').find('gazebo_ros'),
+                    'launch',
+                    'gazebo.launch.py')),
             launch_arguments={'world': world_path}.items()
         )
     ])
